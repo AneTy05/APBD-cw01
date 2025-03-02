e179a66a8bc219e1977fcb6a9cace6192a0dcc18
Aneta Tyrna <s28508@pjwstk.edu.pl>
2025-03-02 16:52:25 +0100

    Zadanie4

--- cw01/cw01/Program.cs
+++ cw01/cw01/Program.cs
@@ -2,3 +2,11 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello U All");
 Console.WriteLine("Bye");
+
+int[] arr = {1, 2, 3, 4, 5};
+
+double getAverage(int[] numbers) {
+    return 0;
+}
+
+getAverage(arr);
